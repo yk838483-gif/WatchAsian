30409d3bd3deda04542ccacc15e7869160fb2865
yk838483-gif <yk838483@gmail.com>
2026-01-14 00:36:00 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
-import express from "express";
-import config from "./config.js";
-import { printWelcome, logger, WELCOME_BANNER } from "./utils/log.js";
-import showsRouter from "./routes/shows.js";
-import otherRouter from "./routes/others.js";
+// ... keep imports and setup code ...
 
-const server = express();
-
-// parse request's body as JSON
-server.use(express.json());
-
-// register routes
 server.use("/", otherRouter)
 server.use("/shows", showsRouter);
 
-server.listen(config.SERVER.PORT, config.SERVER.HOST, () => {
-    printWelcome();
-    logger.info(`Server is running on ${config.SERVER.HOST}:${config.SERVER.PORT}`);
-});
+// --- DELETE THE server.listen(...) PART ---
+// --- PASTE THIS INSTEAD: ---
+
+export default function handler(req, res) {
+    return server(req, res);
+}
